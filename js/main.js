0fa5a6a7c2816f26bb3d01ee223910d631d46fef
CINEMAX-MV <cinemax544@gmail.com>
2026-02-14 22:20:31 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,70 @@
 function toggleMenu(){
-let sidebar = document.getElementById("sidebar");
-
-sidebar.style.left =
-sidebar.style.left === "0px" ? "-250px" : "0px";
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
 }
 
-/* Load Movies */
+/* ===== Pagination Variables ===== */
+let currentPage = 1;
+const moviesPerPage = 12;
+let moviesData = [];
 
+/* ===== Load Movies from JSON ===== */
 fetch("data/movies.json")
 .then(res => res.json())
 .then(data => {
-
-let movieList = document.getElementById("movieList");
-
-data.forEach((movie,index)=>{
-
-movieList.innerHTML += `
-<div class="movie-card"
-onclick="openMovie(${index})">
-
-<img src="${movie.image}">
-<h4>${movie.title}</h4>
-
-</div>
-`;
-
+    moviesData = data;
+    displayMovies();
+    updateButtons();
 });
 
-});
+/* ===== Display Movies Function ===== */
+function displayMovies(){
+    const movieList = document.getElementById("movieList");
+    movieList.innerHTML = "";
 
+    const start = (currentPage - 1) * moviesPerPage;
+    const end = start + moviesPerPage;
+    const paginatedMovies = moviesData.slice(start, end);
+
+    paginatedMovies.forEach((movie,index) => {
+        movieList.innerHTML += `
+            <div class="movie-card" onclick="openMovie(${start + index})">
+                <img src="${movie.image}" alt="${movie.title}">
+                <h4>${movie.title}</h4>
+            </div>
+        `;
+    });
+}
+
+/* ===== Open Movie Function ===== */
 function openMovie(id){
-window.location.href = `movie.html?id=${id}`;
+    window.location.href = `movie.html?id=${id}`;
+}
+
+/* ===== Pagination Buttons ===== */
+function changePage(direction){
+    const totalPages = Math.ceil(moviesData.length / moviesPerPage);
+    currentPage += direction;
+
+    if(currentPage < 1) currentPage = 1;
+    if(currentPage > totalPages) currentPage = totalPages;
+
+    displayMovies();
+    updateButtons();
+}
+
+/* ===== Update Buttons ===== */
+function updateButtons(){
+    const totalPages = Math.ceil(moviesData.length / moviesPerPage);
+
+    const pageInfo = document.getElementById("pageInfo");
+    if(pageInfo){
+        pageInfo.innerText = `Page ${currentPage} of ${totalPages}`;
+    }
+
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+
+    if(prevBtn) prevBtn.disabled = currentPage === 1;
+    if(nextBtn) nextBtn.disabled = currentPage === totalPages;
 }
