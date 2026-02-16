72cbc000f6507db3ff0cb608f587979fdd2d1029
CINEMAX-MV <cinemax544@gmail.com>
2026-02-16 23:42:59 +0530

    Update og.js

--- api/og.js
+++ api/og.js
@@ -1,22 +1,29 @@
 export default function handler(req, res) {
 
-  const { title, image } = req.query;
+  const { id, title, image } = req.query;
 
   res.setHeader("Content-Type", "text/html");
 
   res.send(`
-    <html>
-      <head>
-        <meta property="og:title" content="${title}" />
-        <meta property="og:image" content="${image}" />
-        <meta property="og:type" content="video.movie" />
-      </head>
-      <body>
-        Redirecting...
-        <script>
-          window.location.href = "/";
-        </script>
-      </body>
-    </html>
+  <html>
+  <head>
+  
+  <meta property="og:title" content="${title}" />
+  <meta property="og:description" content="Watch ${title} Full Movie Online - CINEMAX LK" />
+  <meta property="og:image" content="${image}" />
+  <meta property="og:url" content="https://cinemaxlk.vercel.app/movie.html?id=${id}" />
+  <meta property="og:type" content="website" />
+
+  <!-- WhatsApp friendly -->
+  <meta name="twitter:card" content="summary_large_image">
+
+  </head>
+
+  <body>
+  <script>
+  window.location.href="/movie.html?id=${id}";
+  </script>
+  </body>
+  </html>
   `);
 }
