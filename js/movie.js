74c091445011b469bca38fa1c316f60454744997
CINEMAX-MV <cinemax544@gmail.com>
2026-03-18 07:39:27 +0530

    Update movie.js

--- js/movie.js
+++ js/movie.js
@@ -54,16 +54,16 @@ fetch("data/movies.json")
 let currentURL = encodeURIComponent(window.location.href);
 
 let movieLink = "";
-let command = ".download "; // default
+let command = "/START DOWNLOAD "; // default
 
 if(movie.players && movie.players.length > 0){
   movieLink = movie.players[0].link;
 
   if(/drive\.google\.com/.test(movieLink)){
-    command = ".gdrive ";
+    command = "/START GDRIVE ";
   }
   else if(/mega\.nz/.test(movieLink)){
-    command = ".mega ";
+    command = "/START MEGA ";
   }
 }
 
